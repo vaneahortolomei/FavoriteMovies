29d73b9deb394d1a548c2632b83d1ce3d5c1956d
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-10-23 14:53:58 +0300

    install scss,basic configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3737
   }
-})
-
-
-// NETLIFY_AUTH_TOKEN: zJvXBlo6SkVJtmfDqBBFDOgJ9QJXRrrPVS7-sfZ-YTA
-// NETLIFY_SITE_ID: d6a2b6b3-17e0-4ad7-afa3-49885d834b79
+});
