481165d80fc61e3c0135279665cc536297eb9d74
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-10-23 12:38:15 +0300

    fix actions

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,7 @@ export default defineConfig({
     port: 3737
   }
 })
+
+
+// NETLIFY_AUTH_TOKEN: zJvXBlo6SkVJtmfDqBBFDOgJ9QJXRrrPVS7-sfZ-YTA
+// NETLIFY_SITE_ID: d6a2b6b3-17e0-4ad7-afa3-49885d834b79
